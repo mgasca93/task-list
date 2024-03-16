18db809d555e284e2a4390b02710263c3ac59f74
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2024-03-16 14:17:01 -0600

    Se modifico la migracion de usuarios con los campos de name, paternale_surnam y maternal surname y se ejecuto la migracion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('paternal_surname');
+            $table->string('maternal_surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
